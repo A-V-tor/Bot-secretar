1110be16836806561cc813fa39edcc427842ef35
Артем Торшин <avtorca4@gmail.com>
2022-07-29 14:41:45 +0300

    bet

---
+++ bot/tkn.py
@@ -0,0 +1,8 @@
+
+h = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+
+wbot = '95eb2fd9767c24b200e08f975956dcd7'
+
+token_bot = '5360018867:AAGJe-ZCOnBP7j-NN0Dot6887tOmmg4p8mk'
+
+USER_ID = 1019918582 
