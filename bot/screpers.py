ca44be4d41cc2f81a80ef0fb973656612961f43d
Артем Торшин <avtorca4@gmail.com>
2022-07-29 23:17:28 +0300

    bet

--- bot/screpers.py
+++ bot/screpers.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
 from datetime import date, datetime
-from bot.tkn import h
+from bot.tkn import h, wbot
 
 
 lst = ['MU?p=MU&.tsrc=fin-srch', 'BYND?p=BYND&.tsrc=fin-srch', 'EXEL?p=EXEL&.tsrc=fin-srch']
@@ -90,4 +90,40 @@ def get_price_crypto():
     for i in data:
         mytable.add_row(i)
     return f'<pre>{mytable}</pre>'
+
+token = wbot
+
+
+def get_weather(a=52.6, b=39.57):
+
+    params = {f'lat': {a}, 'lon': {b}, 'appid': token, 'units': 'metric', 'lang': 'ru'}
+    response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=params)
+    res = response.json()
+
+    # вынимаю из словаря нужные данные
+    locality = res['name']
+    description = res['weather'][0]['description']
+    temp = (res['main']['temp_min'] + res['main']['temp_min'])/2
+    humidity = res['main']['humidity']
+    pressure = res['main']['pressure']
+    speed_wind = res['wind']['speed']
+    gust_wind = res['wind']['gust']
+
+    if description == 'пасмурно':
+        g = '\u2600'
+    if description == 'небольшой дождь':
+        g = '\u2602'
+    else:
+        g = '\u2601'
+
+    # добавил эмодзи для лучшего восприятия   
+    p = f'населеный пункт:        \U0001F3D9{locality}\n\
+небо:                               {g}{description}\n\
+средняя температура:\U0001F321{temp}\u2103\n\
+влажность воздуха:    \U0001F30A{humidity}%\n\
+давление:                      \U0001F388{pressure} мм.рт.ст\n\
+скорость ветра:            \U0001F32C{speed_wind} м\с\n\
+порывы:                         \u21A0{gust_wind} м\с'
+
+    return p
 
